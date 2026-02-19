15d0da0fa6dfc189badd60281e6e32a0af283ed4
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-02-19 10:23:57 -0500

    Creation of Exercise Model

--- server/models.py
+++ server/models.py
@@ -6,4 +6,10 @@ from marshmallow import Schema, fields, validate, ValidationError
 metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
-# Define Models here
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    equipment_needed = db.Column(db.boolean, nullable=False)
