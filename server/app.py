1221b844482da420dc1cb82b3338a98a31d06e3f
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-02-19 17:11:07 -0500

    Databese initialized and migration done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db, Author, Post
+from models import db, Exercise, Workout, WorkoutExercises
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
