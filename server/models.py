72c3996ff5334b10065c7a6f80cbbe8ea6c51eec
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-02-19 10:25:58 -0500

    Creation of Workout Model

--- server/models.py
+++ server/models.py
@@ -13,3 +13,11 @@ class Exercise(db.Model):
     name = db.Column(db.String(100), nullable=False)
     category = db.Column(db.String(50), nullable=False)
     equipment_needed = db.Column(db.boolean, nullable=False)
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.String(255))
