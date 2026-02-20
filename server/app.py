2cd95a4bb0119a756589580a4e80b88d8b7b13aa
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-02-19 19:43:22 -0500

    Endpoints created

--- server/app.py
+++ server/app.py
@@ -12,7 +12,50 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# Define Routes here
+@app.route('/')
+def home():
+    return make_response(
+        '<h1>Welcome to the Workout Tracker API</h1><p>Use the endpoints to manage your workouts and exercises.</p>')
+
+@app.route('/workouts', methods=['GET'])
+def get_workouts():
+    pass
+
+@app.route('/workouts/<id>', methods=['GET'])
+def get_workout(id):
+    pass
+
+@app.route('/workouts', methods=['POST'])
+def create_workout():
+    pass
+
+@app.route('/workouts/<id>', methods=['DELETE'])
+def delete_workout(id):
+    pass
+
+@app.route('/exercises', methods=['GET'])
+def get_exercises():
+    pass
+
+@app.route('/exercises/<id>', methods=['GET'])
+def get_exercise(id):
+    pass
+
+@app.route('/exercises', methods=['POST'])
+def create_exercise():
+    pass
+
+@app.route('/exercises', methods=['POST'])
+def create_exercise():
+    pass
+
+@app.route('/exercises/<id>', methods=['DELETE'])
+def delete_exercise(id):
+    pass
+
+@app.route('/workouts/<workout_id>/exercises/<exercise_id>/workout_exercises', methods=['POST'])
+def add_exercise_to_workout(workout_id, exercise_id):
+    pass
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
